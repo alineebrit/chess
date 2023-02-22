b1282f142d561162d02a7ab0239567963a7fe515
alineebrit <aline.neves@ccc.ufcg.edu.br>
2023-02-22 17:05:18 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,8 +8,8 @@ import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
-	
-//https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+	// https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
@@ -35,7 +35,7 @@ public class UI {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
 	}	
-		
+	
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -47,22 +47,35 @@ public class UI {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
 	}
-
-
+	
 	public static void printBoard(ChessPiece[][] pieces) {
-		for(int i =0; i<pieces.length; i++) {
-			System.out.print((8-i) + " ");
-			for(int j =0; j<pieces.length; j++) {
-				printPiece(pieces[i][j]);
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
-	private static void printPiece(ChessPiece piece) {
+
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
     	if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else {
             if (piece.getColor() == Color.WHITE) {
