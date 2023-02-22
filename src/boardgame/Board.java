b16988e1d90520202593c644dcb38ab38a538fbc
alineebrit <aline.neves@ccc.ufcg.edu.br>
2023-02-22 15:14:56 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -53,6 +53,21 @@ public class Board {
 		piece.position = position;
 	}
 	
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+			
+		}
+		
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns; 
 	}
@@ -69,5 +84,6 @@ public class Board {
 		
 		return piece(position) != null;
 	}
+	
 	
 }
